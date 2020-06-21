143134204e0afd07daa3381ef0c89ca12953219b
Gustavo Diniz <gustavodnz035@gmail.com>
2020-06-21 11:19:33 -0300

    Eslint, prettier and nodejs running with env variables

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const dotenv = require('dotenv')
+dotenv.config({ path: '.config/config.env' })
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} monde on port ${PORT} `))
